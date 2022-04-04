1c8ad5a2015954e621af2197d77d623ee0f2d12e
Anna Park <parkannuh@gmail.com>
2022-04-04 23:16:14 +0000

    Listen for input events on photourl to update src attribute.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+var $photoUrl = document.querySelector('#photourl');
+var $image = document.querySelector('img');
+
+$photoUrl.addEventListener('input', function (event) {
+  $image.setAttribute('src', $photoUrl.value);
+  if ($photoUrl.value === '') {
+    $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+});
