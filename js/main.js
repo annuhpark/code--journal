abed5b9a5dbd8a2c12a617e4dd6cb07679ada6b7
Anna Park <parkannuh@gmail.com>
2022-04-05 18:15:07 +0000

    Define a function that takes a single journal entry object and returns a DOM tree.

--- js/main.js
+++ js/main.js
@@ -19,9 +19,34 @@ $form.addEventListener('submit', function (event) {
     title: $title.value,
     photourl: $photoUrl.value,
     notes: $notes.value,
-    nextEntryId: data.nextEntryId++
+    nextEntryId: data.nextEntryId
   };
+  data.nextEntryId++;
   data.entries.unshift(entry);
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntries(journalentry) {
+  var $container = document.createElement('div');
+  $container.setAttribute('class', 'container');
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row padding-bottom');
+  $container.appendChild($row);
+  var $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column-half');
+  $row.appendChild($columnHalf);
+  var $img = document.createElement('img');
+  $img.setAttribute('src', journalentry.photourl);
+  $columnHalf.appendChild($img);
+  var $secondColumnHalf = document.createElement('div');
+  $secondColumnHalf.setAttribute('class', 'column-half');
+  $row.appendChild($secondColumnHalf);
+  var $h2 = document.createElement('h2');
+  $h2.textContent = journalentry.title;
+  $secondColumnHalf.appendChild($h2);
+  var $p = document.createElement('p');
+  $p.textContent = journalentry.notes;
+  $secondColumnHalf.appendChild($p);
+  return $container;
+}
