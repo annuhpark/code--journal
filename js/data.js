ebb0fb57a53693c2bde7c443588b07469660cdc9
Anna Park <parkannuh@gmail.com>
2022-04-05 01:08:40 +0000

    Verify that new entries are being saved onto the local storage along with the rest of the data model on page reload & verify that each entry receives an incrementing entryId.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (event) {
+  var todosJSON = JSON.stringify(data.entries);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+});
+
+var previousEntriesJSON = localStorage.getItem('javascript-local-storage');
+if (previousEntriesJSON !== null) {
+  data.entries = JSON.parse(previousEntriesJSON);
+}
